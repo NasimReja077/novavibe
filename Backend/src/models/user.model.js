11044d47e3ba49b0cf89a9472c330f471cb2a2e5
Nasim <nasim.reja.077@gmail.com>
2026-08-04 23:31:19 +0530

    Refactor user model to use ES6 imports, restructure schema definition, and enhance password hashing functionality

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,22 +1,40 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [ true, "Username is required" ],
-        unique: [ true, "Username must be unique" ]
-    },
     email: {
         type: String,
         required: [ true, "Email is required" ],
         unique: [ true, "Email must be unique" ]
     },
+    username: {
+        type: String,
+        required: [ true, "Username is required" ],
+        unique: [ true, "Username must be unique" ]
+    },
     password: {
         type: String,
         required: [ true, "Password is required" ],
-        select: false
+        // required: function () {
+        //        return !this.googleID;
+        //   }
     }
+    // googleID: {
+    //       type: String,
+    //  }
 })
 
-const userModel = mongoose.model("users", userSchema);
-module.exports = userModel;
+userSchema.pre("save", async function (){
+     if (!this.isModified("password")) return;
+
+     const hash = await bcrypt.hash(this.password, 10);
+     this.password = hash;
+})
+
+userSchema.methods.comparePassword = async function (password){
+     return await bcrypt.compare(password, this.password);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+export default userModel;
