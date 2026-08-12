d3a15cfc64aa18d37ee2de7e49d0cbade4257c5a
Nasim <nasim.reja.077@gmail.com>
2026-08-12 19:59:37 +0530

    Integrate Google OAuth strategy and enable authentication routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,8 +3,13 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
 
-// import authRoutes from "./routes/auth.routes";
-// import songRoutes from "./routes/song.routes";
+import authRoutes from "./routes/auth.routes.js";
+// import songRoutes from "./routes/song.routes.js";
+
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { config } from "./config/config.js";
+
 
 const app = express();
 
@@ -13,6 +18,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(cors({
      origin: "http://localhost:5173",
      methods: [ "GET", "POST", "PUT", "DELETE" ],
@@ -23,11 +29,21 @@ app.use(cors({
  * Routes
  */
 
+app.use(passport.initialize());
+
+passport.use(new GoogleStrategy({
+     clientID: config.GOOGLE_CLIENT_ID,
+     clientSecret: config.GOOGLE_CLIENT_SECRET,
+     callbackURL: "/api/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+     return done(null, profile);
+}))
+
 app.get("/", (_req, res) => {
      res.status(200).json({ message: "Server is Running" });
 });
 
-// app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 // app.use("/api/songs", songRoutes)
 
 export default app;
