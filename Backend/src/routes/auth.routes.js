75f01fa64d49f5b3bea39d595b0d11a57f93195b
Nasim <nasim.reja.077@gmail.com>
2026-08-12 19:28:59 +0530

    Refactor auth routes to include logout and refresh functionality, and restore Google authentication endpoints

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,33 +1,29 @@
 import { Router } from "express";
 import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
-import { register, login, getMe } from "../controllers/auth.controller.js";
-// import { authenticateUser } from "../middlewares/auth.middleware.js";
-// import passport from "passport";
+import { register, login, getMe, logout, googleCallback, refresh } from "../controllers/auth.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
+import passport from "passport";
 import { config } from "../config/config.js";
 
 const router = Router();
 
-router.post('/register', validateRegisterUser, register)
-router.post("/login", validateLoginUser, login)
-// router.get("/google", 
-//      passport.authenticate("google", { scope: [ "profile", "email" ] })
-// )
+router.post('/register', validateRegisterUser, register);
+router.post("/login", validateLoginUser, login);
+router.post("/logout", authenticateUser, logout);
+router.get('/me', authenticateUser, getMe);
 
-// router.get("/google/callback", 
-//      passport.authenticate("google", { 
-//           session: false,
-//           failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
-//      }),
-//      googleCallback,
-// )
+router.post("/refresh", refresh);
 
+router.get("/google", 
+     passport.authenticate("google", { scope: [ "profile", "email" ] })
+)
 
-/**
- * @route GET /api/auth/me
- * @description Get the authenticated user's profile
- * @access Private
- */
-// router.get('/me', authenticateUser, getMe)
-
+router.get("/google/callback", 
+     passport.authenticate("google", { 
+          session: false,
+          failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
+     }),
+     googleCallback,
+)
 
 export default router;
