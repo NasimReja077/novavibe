1e566d31ef2a58b6fdbc00b1cc0e850bad520b0e
Nasim <nasim.reja.077@gmail.com>
2026-08-14 23:42:59 +0530

    feat: implement authApi service for authentication endpoints

---
+++ Frontend/src/features/auth/service/authApi.js
@@ -0,0 +1,9 @@
+import api from "../../Shared/services/api.js";
+
+export const authApi = {
+     register: (data) => api.post("/auth/register", data),
+     login: (data) => api.post("/auth/login", data),
+     logout: () => api.post("/auth/logout"),
+     getMe: () => api.get("/auth/me"),
+     refresh: () => api.post("/auth/refresh")
+}
