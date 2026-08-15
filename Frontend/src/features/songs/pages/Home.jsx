41ac29ec477313bbdbc68dfe2eb3c1a1781bf76d
Nasim <nasim.reja.077@gmail.com>
2026-08-15 23:58:02 +0530

    feat: create Home component to integrate FaceExpression detection

---
+++ Frontend/src/features/songs/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from '../../faceDetectExpression/components/FaceExpression'
+
+function Home() {
+  return (
+    <div>
+      <FaceExpression/>
+    </div>
+  )
+}
+
+export default Home
