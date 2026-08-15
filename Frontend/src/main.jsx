3214ad0c922388e918c19102f3de8a12e6c3b24d
Nasim <nasim.reja.077@gmail.com>
2026-08-15 23:53:07 +0530

    fix: standardize import statements and correct formatting in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './App/index.css';
 import App from './App/App.jsx';
-import { Provider } from 'react-redux'
-import { store } from './app/app.store.js'
-
+import { Provider } from 'react-redux';
+import { store } from './App/app.store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
   </StrictMode>,
-)
+);
