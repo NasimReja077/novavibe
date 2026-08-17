d86f8618d4c4dac2ddf3f11efd014c35ff70c6f2
Nasim <nasim.reja.077@gmail.com>
2026-08-17 15:22:33 +0530

    feat: add reducers for playlists, songs, recently played, bookmarks, and player to the store

--- Frontend/src/App/app.store.js
+++ Frontend/src/App/app.store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/state/auth.slice.js";
+import playlistReducer from "../features/playlists/state/playlist.slice.js"
+import songReducer from "../features/songs/state/song.slice.js"
+import recentlyPlayedReducer from "../features/recentlyPlayed/state/recentlyPlayed.slice.js"
+import bookmarkReducer from "../features/bookmarks/state/bookmark.slice.js"
+import playerReducer from "../features/player/state/player.slice.js"
 
 export const store = configureStore({
      reducer: {
           auth: authReducer,
+          playlists: playlistReducer,
+          songs: songReducer,
+          recentlyPlayed: recentlyPlayedReducer,
+          bookmarks: bookmarkReducer,
+          player: playerReducer,
      }
 })
