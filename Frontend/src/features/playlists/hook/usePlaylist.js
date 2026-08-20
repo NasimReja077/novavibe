ec08de40c00a3c557ed17f59cb0d82a8596eee37
Nasim <nasim.reja.077@gmail.com>
2026-08-20 23:14:14 +0530

    feat: refactor usePlaylists hook to utilize useCallback for improved performance and readability

--- Frontend/src/features/playlists/hook/usePlaylist.js
+++ Frontend/src/features/playlists/hook/usePlaylist.js
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchAllPlaylists,
@@ -13,6 +14,26 @@ export const usePlaylists = () => {
     (state) => state.playlists
   );
 
+  const loadPlaylists = useCallback(() => dispatch(
+    fetchAllPlaylists()), [dispatch]
+  );
+
+  const loadPlaylist = useCallback((id) => dispatch(
+    fetchPlaylistById(id)), [dispatch]
+  );
+
+  const create = useCallback((formData) => dispatch(
+    createPlaylist(formData)), [dispatch]
+  );
+
+  const clearError = useCallback(() => dispatch(
+    clearPlaylistError()), [dispatch]
+  );
+
+  const clearCurrent = useCallback(() => dispatch(
+    clearCurrentPlaylist()), [dispatch]
+  );
+
   return {
     playlists: list,
     currentPlaylist: current,
@@ -20,10 +41,10 @@ export const usePlaylists = () => {
     creating,
     error,
 
-    fetchPlaylists: () => dispatch(fetchAllPlaylists()),
-    fetchPlaylist: (id) => dispatch(fetchPlaylistById(id)),
-    createPlaylist: (formData) => dispatch(createPlaylist(formData)),
-    clearError: () => dispatch(clearPlaylistError()),
-    clearCurrentPlaylist: () => dispatch(clearCurrentPlaylist()),
+    fetchPlaylists: loadPlaylists,
+    fetchPlaylist: loadPlaylist,
+    createPlaylist: create,
+    clearError,
+    clearCurrentPlaylist: clearCurrent,
   };
 };
